0f6b391b717c30bd7f05116919da887509592248
leo113000 <leojulianvazquez@gmail.com>
2017-05-27 01:29:21 -0300

    Clase Sensores

--- src/monitor/Sensores.java
+++ src/monitor/Sensores.java
@@ -11,28 +11,43 @@ import oshi.hardware.Sensors;
  *
  * @author Usuario
  */
-public abstract class Sensores implements iSensores {
-    
+public abstract class Sensores implements iSensores
+{
+
     private Sensors sensor;
 
-    public Sensores(Sensors sensor) {
-        this.sensor = sensor;
-    }
-
-        
-    @Override
-    public Double getTempCPU() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Sensores(Sensors sensor)
+    {
+	this.sensor = sensor;
     }
 
     @Override
-    public Double getVoltajeCPU() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Double getTempCPU()
+    {
+	throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public Integer[] getVelVentiladores() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Double getVoltajeCPU()
+    {
+	throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
+    @Override
+    public Integer[] getVelVentiladores()
+    {
+
+	int[] aux = sensor.getFanSpeeds();
+	Integer[] rta = new Integer[aux.length]; 
+	
+	int i=0;
+	
+	for(int enterito : aux) //a cada entero que nos devuelve la librería
+	{
+	    rta[i]=enterito; //se lo asigna a un espacio de un array de Integer
+	}
+	
+	return rta;
+    }
+
 }
